17dca1b8264c911fd957ddedc7f7ec9e59eabbd3
boytikoy <michaelbayaras@gmail.com>
2023-10-11 09:04:12 +0800

    Deploy success

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -10,6 +10,11 @@ namespace API.Data
 {
     public class Seed
     {
+        public static async Task ClearConnections(DataContext context)
+        {
+            context.Connections.RemoveRange(context.Connections);
+            await context.SaveChangesAsync();
+        }
         public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
             if (await userManager.Users.AnyAsync()) return;
@@ -33,6 +38,8 @@ namespace API.Data
             foreach (var user in users)
             {
                 user.UserName = user.UserName.ToLower();
+                user.Created = DateTime.SpecifyKind(user.Created, DateTimeKind.Utc);
+                user.LastActive = DateTime.SpecifyKind(user.LastActive, DateTimeKind.Utc);
                 await userManager.CreateAsync(user, "1234");
                 await userManager.AddToRoleAsync(user, "Member");
             }
